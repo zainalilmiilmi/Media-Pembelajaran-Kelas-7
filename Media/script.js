c07f858cf5b946d4e038f40a163b47d2c3700cae
zainal ilmi <131847184+zainalilmiilmi@users.noreply.github.com>
2024-06-24 15:33:08 +0800

    Delete Media directory

--- Media/script.js
+++
@@ -1,8 +0,0 @@
-function checkAnswer(answer) {
-    if (answer === 'correct') {
-        alert('Jawaban benar!');
-        window.location.href = 'conclusion.html';
-    } else {
-        alert('Jawaban salah, coba lagi.');
-    }
-}
